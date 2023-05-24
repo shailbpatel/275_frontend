6fa956bc2c09d2da3b269648d0cc84ea7526629a
Shail Patel <shailbrijeshkumar.patel@sjsu.edu>
2023-05-24 05:26:44 +0000

    edited

--- src/Components/Utils/urlConfig.js
+++ src/Components/Utils/urlConfig.js
@@ -1,5 +1,5 @@
-export const dynamicURL = "http://52.23.102.141:8080";
-export const backendURL = "http://52.23.102.141:8080";
+export const dynamicURL = "http://www.woirs-cmpe275.com/:8080";
+export const backendURL = "http://www.woirs-cmpe275.com/:8080";
 
 export const clientId = "343518867487-hbofr8ntpbnr18mrrja6f1d7aso6rk5u.apps.googleusercontent.com";
 
